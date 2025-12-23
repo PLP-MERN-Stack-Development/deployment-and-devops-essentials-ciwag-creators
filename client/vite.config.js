a890324775359f53c31a087c0022cac92103b733
ciwag-creators <ciwag255@gmail.com>
2025-12-23 19:45:35 +0100

    working on 404

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // important for correct routing on Vercel
   server: {
     port: 5173,
     proxy: {
